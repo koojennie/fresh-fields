56cc099f4e336a7296f69f3ff70e68271ecf3607
Alia Jennifer <aliyajenny14@gmail.com>
2024-07-23 21:00:35 +0700

    uploading

---
+++ main.js
@@ -0,0 +1,47 @@
+// navbar
+function toggleMenu() {
+  const toggleMenu = document.querySelector(".toggleMenu");
+  const navigation = document.querySelector(".navigation");
+  toggleMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
+
+// swiper slider 1
+var swiper = new Swiper(".trend-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".trend-slider .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  // swiper slider 2
+  var mySwiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+    },
+  });
